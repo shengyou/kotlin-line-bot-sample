d27ee416c72ff89d0ccc27cbb295194e5d6bf38d
Shengyou Fan <shengyoufan@gmail.com>
2021-03-13 19:36:25 +0800

    add line bot sdk

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.linecorp.bot:line-bot-spring-boot:4.3.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 

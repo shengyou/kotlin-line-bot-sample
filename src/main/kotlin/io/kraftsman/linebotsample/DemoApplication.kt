a4b492a17f6babecd75c1dd906b274a307ba0893
Shengyou Fan <shengyoufan@gmail.com>
2021-03-13 19:26:08 +0800

    project init

---
+++ src/main/kotlin/io/kraftsman/linebotsample/DemoApplication.kt
@@ -0,0 +1,11 @@
+package io.kraftsman.linebotsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
---
+++ src/test/kotlin/io/kraftsman/linebotsample/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.kraftsman.linebotsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

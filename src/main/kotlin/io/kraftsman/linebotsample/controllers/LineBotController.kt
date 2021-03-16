784582e4341433444ea38b944095f96bf954bfb9
Shengyou Fan <shengyoufan@gmail.com>
2021-03-16 12:49:50 +0800

    monitoring text

--- src/main/kotlin/io/kraftsman/linebotsample/controllers/LineBotController.kt
+++ src/main/kotlin/io/kraftsman/linebotsample/controllers/LineBotController.kt
@@ -1,22 +1,33 @@
 package io.kraftsman.linebotsample.controllers
 
+import com.linecorp.bot.client.LineMessagingClient
+import com.linecorp.bot.model.ReplyMessage
 import com.linecorp.bot.model.event.MessageEvent
 import com.linecorp.bot.model.event.message.TextMessageContent
-import com.linecorp.bot.model.message.Message
 import com.linecorp.bot.model.message.TextMessage
 import com.linecorp.bot.spring.boot.annotation.EventMapping
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler
-import io.kraftsman.linebotsample.DemoApplication
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 
 @LineMessageHandler
-class LineBotController {
-    private val log = LoggerFactory.getLogger(DemoApplication::class.java)
+class LineBotController(
+    @Autowired val lineMessagingClient: LineMessagingClient,
+) {
+    private val log = LoggerFactory.getLogger(LineBotController::class.java)
 
     @EventMapping
-    fun handleTextMessageEvent(event: MessageEvent<TextMessageContent>): Message {
-        log.info("event: $event")
-        val originalMessageText = event.message.text
-        return TextMessage(originalMessageText)
+    fun handleTextMessageEvent(event: MessageEvent<TextMessageContent>) {
+        when (event.message.text) {
+            "ping" -> {
+                val apiResponse = lineMessagingClient.replyMessage(
+                    ReplyMessage(
+                        event.replyToken,
+                        listOf(TextMessage("Pong"))
+                    )
+                ).get()
+                log.info("Sent messages: {}", apiResponse)
+            }
+        }
     }
 }
